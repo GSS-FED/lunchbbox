260d157d28eaf170f9d1362f42a8b6839ca5af18
jj811208 <jj811208@gmail.com>
2019-08-07 14:08:07 +0800

    two commit

---
+++ build/precache-manifest.54bee542cde8da03680f0a6638111798.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3440777d52f75f16d2e68ce842ea4f83",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b7f4f489aa8927161607",
+    "url": "./static/css/main.af094bff.chunk.css"
+  },
+  {
+    "revision": "3d8fb4e0a3ea41f2c4a6",
+    "url": "./static/js/2.d1edc178.chunk.js"
+  },
+  {
+    "revision": "b7f4f489aa8927161607",
+    "url": "./static/js/main.1879f9e6.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
